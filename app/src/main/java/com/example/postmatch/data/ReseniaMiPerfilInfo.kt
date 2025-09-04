edb34072f4a3e0b00b20142560d29d9efbc6ea78
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2025-09-04 12:31:38 -0500

    Reviews unificadas

--- app/src/main/java/com/example/postmatch/data/ReseniaMiPerfilInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.postmatch.data
-
-data class ReseniaMiPerfilInfo(
-    var id: Int,
-    var nEstrellas: Int,
-    var tituloReseniaPerfil: String,
-    var descripcionReseniaPerfil: String,
-    var idFoto: Int
-)
