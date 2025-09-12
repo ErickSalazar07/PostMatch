adf9af0024b66eaa226c963f31b0c2d548f14a26
juanedudiazsoft12 <88118954+juanealop@users.noreply.github.com>
2025-09-12 11:18:21 -0500

    Create SplashScreen.kt

---
+++ app/src/main/java/com/example/postmatch/ui/splash/SplashScreen.kt
@@ -0,0 +1,46 @@
+package com.example.postmatch
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.postmatch.navigation.AppNavigation
+import androidx.compose.runtime.*
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.postmatch.navigation.BottomNavBar
+
+
+@Composable
+fun PostMatchApp() {
+    val navController = rememberNavController()
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination?.route
+    val showBottomBar = currentDestination !in listOf("login", "registro","splash")
+
+    Scaffold (
+        bottomBar = {
+            if (showBottomBar) {
+                BottomNavBar(
+                    selectedRoute = currentDestination ?: "publicaciones",
+                    onItemClick = { route ->
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+
+    ){
+
+        AppNavigation(
+            navController = navController,
+            modifier = Modifier.padding(it)
+        )
+        }
+
+
+}
