be48650b879280e9259c45916f7293d914c543bb
gyusba <f-garrido@javeriana.edu.co>
2025-10-02 18:34:15 -0500

    Retrofit para reviews en home

---
+++ app/src/main/java/com/example/postmatch/data/datasource/services/ReviewRetrofitService.kt
@@ -0,0 +1,28 @@
+package com.example.postmatch.data.datasource.services
+
+import com.example.postmatch.data.dtos.CreateReviewDto
+import com.example.postmatch.data.dtos.ReviewDto
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ReviewRetrofitService {
+
+    @GET("/Reviews")
+    suspend fun getAllReviews(): List<ReviewDto>
+
+    @GET("/Reviews{id}")
+    suspend fun getReviewById(@Path("id") id: Int): ReviewDto
+
+    @POST("/Reviews")
+    suspend fun createReview(@Body review: CreateReviewDto): Unit
+
+    @DELETE("/Reviews/{id}")
+    suspend fun deleteReview(@Path("id") id: Int): Unit
+
+    @PUT("/Review/{id}")
+    suspend fun updateReview(@Path("id")id: Int, @Body review: CreateReviewDto): Unit
+}
---
+++ app/src/main/java/com/example/postmatch/data/injection/AppModule.kt
@@ -0,0 +1,38 @@
+package com.example.postmatch.data.injection
+
+import androidx.compose.ui.layout.ScaleFactor
+import com.example.postmatch.data.datasource.services.ReviewRetrofitService
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesRetroFit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:3000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesReviewRetrofitService(retrofit: Retrofit): ReviewRetrofitService {
+        return retrofit.create(ReviewRetrofitService::class.java)
+    }
+}
