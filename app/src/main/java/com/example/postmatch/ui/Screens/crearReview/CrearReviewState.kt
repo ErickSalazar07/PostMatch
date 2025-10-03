dd85a61fffc8c459a2299a70cc3ba7c6ffcc93d9
gyusba <f-garrido@javeriana.edu.co>
2025-10-02 23:17:53 -0500

    Crear review

--- app/src/main/java/com/example/postmatch/ui/Screens/crearReview/CrearReviewState.kt
+++ app/src/main/java/com/example/postmatch/ui/Screens/crearReview/CrearReviewState.kt
@@ -5,7 +5,10 @@ import com.example.postmatch.data.local.LocalPartidoProvider
 
 data class CrearReviewState(
     val resenha: String = "",
+    val titulo: String = "",   // 👈 nuevo campo
     val partido: PartidoInfo = PartidoInfo(),
     val calificacion: Int = 1,
     val publicarButtonClick: () -> Unit = {}
 )
+
+
