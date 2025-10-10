69ba5af127013a7cb6d77d49dd4326e1644cbd8c
Erick Salazar <ericksalazar@javeriana.edu.co>
2025-10-09 19:04:50 -0500

    fix: se pasa de retrofit a firebase en perfil

--- app/src/main/java/com/example/postmatch/data/injection/FirebaseHiltModule.kt
+++ app/src/main/java/com/example/postmatch/data/injection/FirebaseHiltModule.kt
@@ -3,12 +3,14 @@ package com.example.postmatch.data.injection
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -18,4 +20,11 @@ class FirebaseHiltModule {
 
     @Provides
     fun storage(): FirebaseStorage = Firebase.storage
+
+    @Provides
+    @Singleton
+    fun providesFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
 }
